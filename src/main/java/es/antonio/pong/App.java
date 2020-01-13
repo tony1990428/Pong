2d218c980d1aed3935407afdd80281931cf10e3b
1º DAW Antonio <1 DAW Antonio@DESKTOP-F42NNUP>
2020-01-13 10:27:00 +0100

    Bola rebotando

--- src/main/java/es/antonio/pong/App.java
+++ src/main/java/es/antonio/pong/App.java
@@ -11,12 +11,16 @@ import javafx.stage.Stage;
  * JavaFX App
  */
 public class App extends Application {
-
+    int ballCenterX = 500;
+    int ballCurrentSpeedX = 3;
+    int ballIncSpeed 
     @Override
     public void start(Stage stage) {
         
+//      StackPane root = new StackPane();
         StackPane root = new StackPane();
         var scene = new Scene(root,640,480);
+        scene.setFill(color.BLACK);
         var javafxVersion = SystemInfo.javafxVersion();
 
         var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
@@ -28,12 +32,33 @@ public class App extends Application {
         circleBall.setCenterX(10);
         circleBall.setCenterY(30);
         circleBall.setRadius(7);
+        circleBall.setfill(color.WHITE);
         
         //Circle circleBall = new circle(10,30,7);
         
         root.getChildren().add(circleBall);
+        
+        Timeline timeline = new Timeline(
+            // 0.017 ~= 60 FPS
+            new KeyFrame(Duration.seconds(0.017), new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent ae) {
+                    int ballCenterX = 10;
+                    circleBall.setCenterX(ballCenterX);
+                    ballCenterX++;
+                    if(ballCenterX == 640) {
+                        ballCurrentSpeedX = -3;
+                        else if(ballCenterX <= 0){
+                        
+                                }
+                        ballCenterX--;
+                    System.out.println("Probando animación");
+                     }
+            })                
+        );
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();        
+                }
     }
-
     public static void main(String[] args) {
         launch();
     }
